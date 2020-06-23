f1cab4fd4aab3eb13aab8d465af19c2d7da2080d
prashant280920 <jayantmehta8447442229@gmail.com>
2020-06-23 20:15:47 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controlers/register');
+const signin = require('./controlers/signIn');
+const profile = require('./controlers/profile');
+const image = require('./controlers/image');
+
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'Prashant@2000',
+    database : 'smart_brain'
+  }
+});
+
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors())
+
+
+app.get("/",(req, res) =>{res.send(database.users)})
+app.post('/signin',(req, res) => { signin.handleSignIn(req, res, db, bcrypt) })
+app.post('/register',(req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id',(req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image',(req, res) => { image.handleImage(req, res, db)} )
+
+
+
+app.listen(3001, () => {
+	console.log("app is reunning");
+})
+
+/*	
+/ --> res = this is working
+/signIn --> POST = success/fail
+/register --> Post = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+
+*/
